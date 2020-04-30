38de946220e70698dbe7fe4763ef14848241b17e
mics <michal.strbka@gmail.com>
2020-04-30 11:38:49 +0200

    PolygonIntersections

--- Intersections/Tests/SvgExport.cs
+++ Intersections/Tests/SvgExport.cs
@@ -7,6 +7,47 @@ namespace Tests
 {
     internal static class SvgExport
     {
+        public static string ToSvg(
+            this IEnumerable<IReadOnlyCollection<PolygonIntersection.Point>> polygons,
+            IEnumerable<PolygonIntersection.Point> intersections)
+        {
+            var allSegments = polygons
+                .SelectMany(points =>
+                {
+                    var polygonSegments = new List<S>();
+                    for (var i = 0; i < points.Count; i++)
+                    {
+                        var p1 = points.ElementAt(i);
+                        var p2 = points.ElementAt((i + 1 ) % points.Count);
+
+                        var s = new S 
+                        {
+                            Name = i.ToString(),
+                            A = new P
+                            {
+                                X = (long)p1.X,
+                                Y = (long)p1.Y
+                            },
+                            B = new P
+                            {
+                                X = (long)p2.X,
+                                Y = (long)p2.Y
+                            },
+                        };
+
+                        polygonSegments.Add(s);
+                    }
+                    return polygonSegments;
+                })
+                .ToArray();
+
+            var ip = intersections
+                .Select(i => new P { X = (long)i.X, Y = (long)i.Y })
+                .ToArray();
+
+            return allSegments.ToSvg(ip);
+        }
+
         public static string ToSvg(this IEnumerable<Segment> segments)
         {
             var svg = segments
@@ -16,49 +57,7 @@ namespace Tests
                     A = new P { X = s.A.X, Y = s.A.Y },
                     B = new P { X = s.B.X, Y = s.B.Y },
                 })
-                .ToSvg();
-
-            //var allPoints = segments
-            //    .SelectMany(s => new[] { s.A, s.B })
-            //    .ToArray();
-
-            //var minX = allPoints
-            //    .Min(s => s.X);
-            //var minY = allPoints
-            //    .Min(s => s.Y);
-
-            //var maxX = allPoints
-            //    .Max(s => s.X);
-            //var maxY = allPoints
-            //    .Max(s => s.Y);
-
-            //var width = Math.Abs(maxX - minX);
-            //var height = Math.Abs(maxY - minY);
-
-            //var output = $"<svg viewbox='{minX - 1} {height - maxY - 1} {width + 2} {height + 2}' widht='500' height='500'>"
-            //    + Environment.NewLine
-            //    + $"<line x1='{minX - 1}' y1='{height - minY + 1}' x2='{minX - 1}' y2='{height - maxY + 1}' style='stroke: rgb(0, 200, 0); stroke-width:0.5;'></line>"
-            //    + Environment.NewLine
-            //    + $"<line x1='{minX - 1}' y1='{height - minY + 1}' x2='{width + 2}' y2='{height - minY + 1}' style='stroke: rgb(0, 200, 0); stroke-width:0.5;'></line>"
-            //    + Environment.NewLine
-            //    + Environment.NewLine
-            //    ;
-
-            //foreach (var segment in segments)
-            //{
-            //    var a = segment.A;
-            //    var b = segment.B;
-            //    output += $"<line x1='{a.X}' y1='{height - a.Y + 1}' x2='{b.X}' y2='{height + 1 - b.Y}' style='stroke: rgb(0, 0, 0); stroke-width:0.5'>"
-            //        + Environment.NewLine
-            //        + $"<title>S{segment.Id}: {a} | {b}</title>"
-            //        + Environment.NewLine
-            //        + "</line>"
-            //        + Environment.NewLine
-            //        ;
-
-            //}
-
-            //output += "</svg>";
+                .ToSvg(new P[0]);
 
             return svg;
         }
@@ -72,15 +71,16 @@ namespace Tests
                     A = new P { X = s.A.X, Y = s.A.Y},
                     B = new P { X = s.B.X, Y = s.B.Y},
                 })
-                .ToSvg();
+                .ToSvg(new P[0]);
 
             return svg;
         }
 
-        private static string ToSvg(this IEnumerable<S> segments)
+        private static string ToSvg(this IEnumerable<S> segments, IReadOnlyCollection<P> points)
         {
             var allPoints = segments
                 .SelectMany(s => new[] { s.A, s.B })
+                .Union(points)
                 .ToArray();
 
             var minX = allPoints
@@ -116,12 +116,35 @@ namespace Tests
                     + "</line>"
                     + Environment.NewLine
                     ;
+            }
 
+            var r = 0.5;
+            if(points.Count > 10)
+            {
+                r = 2;
+            }
+
+            foreach(var point in points)
+            {
+                output += $"<circle cx='{point.X}' cy='{height - point.Y + 1}' r='{r}' fill='red'>"
+                    + Environment.NewLine
+                    + $"<title>{point.X},{point.Y}</title>"
+                    + Environment.NewLine
+                    + "</circle>"
+                    + Environment.NewLine;
             }
 
             output += "</svg>";
 
-            return output;
+            return "<html>"
+                + Environment.NewLine
+                + "<head></head>"
+                + Environment.NewLine
+                + "<body>"
+                + Environment.NewLine
+                + output
+                + Environment.NewLine
+                + "</body>";
         }
 
         private class P
@@ -136,6 +159,5 @@ namespace Tests
             public P A { get; set; }
             public P B { get; set; }
         }
-
     }
 }
